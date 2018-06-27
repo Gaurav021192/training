1212c0417d14317e36184f0830c99cfacb1a1e45
Gaurav Bisht <gaurav.bisht1@T-Mobile.com>
2018-06-27 21:13:28 +0530

    fixes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
-import { AppModule } from '../app.module';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [HomeComponent]
+  declarations: []
 })
 export class HomeModule { }
